50b702c0040093b46d6fd9885eb31f2960b92021
CodinaFJ <fjavier.codina@gmail.com>
2023-01-06 10:39:25 +0100

    Bug found in strnstr

--- libc/ft_strnstr.c
+++ libc/ft_strnstr.c
@@ -1,6 +1,7 @@
 #include "libft.h"
 
-//I hesitate if len applies to needle or haystack. Right now it is applied to needle, because I am lazy and it was easier (I think it makes more sense for it to be applied to haystack)
+//I hesitate if len applies to needle or haystack. Right now it is applied to haystack.
+//Still, the comparison in the while loop can make get further than len and then it wouldn't work. This is a bug that MUST be fixed.
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	char	*s;
@@ -9,10 +10,10 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	if (*needle == '\0')
 		return (haystack);
 	s = haystack;
-	while (s != NULL)
+	while (s != NULL && (int)(s - haystack) < (int)len)
 	{
-		s = strchr(haystack, (int) *needle); 
-		if (ft_memcmp(s, needle, len) == 0)
+		s = strchr(haystack, (int)*needle); 
+		if (ft_memcmp(s, needle, ft_strlen(needle)) == 0)
 			return (s);
 	}
 	return (s);
